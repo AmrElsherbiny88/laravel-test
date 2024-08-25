6cc6b567cfd407512b1200efbb22e0c76bf552aa
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-08-25 10:57:14 +0300

    para. routing

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/users/{name}', function ($name) {
+    return $name;
 });
